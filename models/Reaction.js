b0ede90d047735d95fb144fcfe3d545901ae0e4d
Steve Bendrick <steven.bendrick@gmail.com>
2021-10-28 19:24:14 -0400

    added reaction model

---
+++ models/Reaction.js
@@ -0,0 +1,36 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+// create User model
+class Reaction extends Model {}
+
+Reaction.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    react: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    post_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'post',
+        key: 'id'
+      }
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'user',
+        key: 'id'
+      }
+    }
+  }
+)
+
+module.exports = Reaction;
--- models/index.js
+++ models/index.js
@@ -1,8 +1,9 @@
 const User = require('./User');
 const Post = require('./Post');
 const Tag = require('./Tag');
-const Comment = require('./Comment')
+const Comment = require('./Comment');
+const Reaction = require('./Reaction');
 
 // belongsTo/hasX code goes here
 
-module.exports = { User, Post, Tag, Comment };
+module.exports = { User, Post, Tag, Comment, Reaction };
