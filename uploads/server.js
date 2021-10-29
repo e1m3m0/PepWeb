962a3db52969736a426bd2a491e9ed10d6c477b7
JessicaLDaley <jessicadaleypaints@yahoo.com>
2021-10-29 17:45:46 -0400

    Set up s3 bucket

--- uploads/server.js
+++ uploads/server.js
@@ -3,12 +3,17 @@ const express = require('express')
 const multer  = require('multer')
 const upload = multer({ dest: 'uploads/' })
 
+const { uploadFile } = require("./s3")
+
 const app = express()
 const PORT = process.env.PORT || 3001;
 
-app.post("/images", upload,single("image"), (req, res) => {
+app.post("/images", upload,single("image"), async (req, res) => {
     const file = req.file
     console.log(file)
+    const result = await uploadFile(file)
+    console.log(result)
+    await uploadFile(file)
     const decription = req.body.description
     res.send("🔥")
 })
