e04529449789585874b715e06fd97aa32ceb2d43
Steve Bendrick <steven.bendrick@gmail.com>
2021-10-28 20:06:45 -0400

    added sequelize index

--- models/index.js
+++ models/index.js
@@ -4,6 +4,52 @@ const Tag = require('./Tag');
 const Comment = require('./Comment');
 const Reaction = require('./Reaction');
 
-// belongsTo/hasX code goes here
+User.hasMany(Post, {
+  foreignKey: 'user_id'
+});
+
+Post.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+Post.hasMany(Comment, {
+  foreignKey: 'post_id'
+});
+
+Comment.hasOne(Post, {
+  foreignKey: 'post_id'
+});
+
+User.hasMany(Comment, {
+  foreignKey: 'user_id'
+});
+
+Comment.hasOne(User, {
+  foreignKey: 'user_id'
+});
+
+User.hasMany(Reaction, {
+  foreignKey: 'user_id'
+});
+
+Reaction.hasMany(User, {
+  foreignKey: 'user_id'
+});
+
+Tag.hasMany(Post, {
+  foreignKey: 'tag_id'
+});
+
+Post.belongsTo(Tag, {
+  foreignKey: 'tag_id'
+});
+
+Reaction.hasMany(Post, {
+  foreignKey: 'post_id'
+});
+
+Post.hasMany(Reaction, {
+  foreignKey: 'post_id'
+});
 
 module.exports = { User, Post, Tag, Comment, Reaction };
