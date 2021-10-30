80434c0ae8a47d30f759264ad2c589c1814862e3
JessicaLDaley <jessicadaleypaints@yahoo.com>
2021-10-30 11:44:50 -0400

    Created test html

--- uploads/server.js
+++ uploads/server.js
@@ -1,4 +1,9 @@
 const express = require('express')
+const sequelize = require('./config/connection');
+
+// const fs = require("fs")
+// const util = require("util")
+// const unlinkFile = utils.promisify(fs.unlink)
 
 const multer  = require('multer')
 const upload = multer({ dest: 'uploads/' })
@@ -6,16 +11,30 @@ const upload = multer({ dest: 'uploads/' })
 const { uploadFile } = require("./s3")
 
 const app = express()
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3306;
+
+app.get('/images/:key', (req, res) => {
+  console.log(req.params)
+  const key = req.params.key
+  const readStream = getFileStream(key)
+
+  readStream.pipe(res)
+})
 
 app.post("/images", upload,single("image"), async (req, res) => {
     const file = req.file
     console.log(file)
     const result = await uploadFile(file)
+    // await unlinkFile(file.path)
+    
+    // apply filter
+  // resize 
+
+    const result = await uploadFile(file)
     console.log(result)
     await uploadFile(file)
     const decription = req.body.description
-    res.send("🔥")
+    res.send({imagePath: `/images/${results.Key}`})
 })
 
 app.listen(PORT, () => 
