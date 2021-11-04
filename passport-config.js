035ff0d5e80612eb9bc52d44e2c7ccc2d123aa79
Antonio Madureira <antoniomadureira2222@gmail.com>
2021-11-03 22:11:19 -0400

    switched from passport-custom to passport-local to fix errors occuring due to undefined functions

--- passport-config.js
+++ passport-config.js
@@ -1,24 +1,25 @@
-const CustomStrategy = require("passport-custom").Strategy
+const LocalStrategy = require("passport-local").Strategy
 const bcrypt = require("bcrypt")
 
 function initialize(passport, getUserEmail, getUserById) {
-    async function authenticateUser(email, password, done) {
+    const authenticateUser = async (email, password, done) => {
         const user = getUserEmail(email);
         if (user == null) {
-            return done(null, false, { message: " EMAIL not associated with any user" });
+            return done(null, false, { message: " EMAIL not associated with any user" })
         }
         try {
             if (await bcrypt.compare(password, user.password)) {
-                return done(null, user);
+                return done(null, user)
             } else {
-                return done(null, false, { message: "Incorrect Password try again" });
+                return done(null, false, { message: "Incorrect Password try again" })
             }
-        } catch (err) {
-            return done(err);
+        } catch (e) {
+            return done(e);
         }
     
     }
-    passport.use( "custome-strategy", new CustomStrategy ({ usernameField: "email" }, authenticateUser))
+    passport.use(new LocalStrategy({ usernameField: "email"}, 
+    authenticateUser))
     passport.serializeUser((user, done) => done(null, user.id))
     passport.deserializeUser((id, done) => {
          return done(null, getUserById(id))
--- server.js
+++ server.js
@@ -1,41 +1,54 @@
+if (process.env.NODE_ENV !== "production" ) {
+    require("dotenv").config()
+}
+
 const express = require("express")
 const app = express()
 const bcrypt = require("bcrypt")
-const passport = require("passport-custom")
+const passport = require("passport")
 const flash = require("express-flash")
+const session = require("express-session")
 const methodOverride = require("method-override")
 
+
 const initializePassport = require("./passport-config")
-
-
-initializePassport (passport, email => {
-    users.find(user => user.email === email),
+initializePassport (
+    passport, 
+    email => users.find(user => user.email === email),
     id => users.find(user => user.id === id)
-})
+)
 
 const users = []
 
 app.set("view-engine", "ejs" )
 app.use(express.urlencoded({ extended: false }))
 app.use(flash())
-app.use(methodOverride ("_method"))
-//app.use(passport.initialize())
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false
+}))
 
-app.get("/", checkNotAuthenticated, (req, res) => {
-    res.render("index.ejs" , {name: "PEPWEB"})
+
+app.use(passport.initialize())
+app.use(passport.session())
+app.use(methodOverride ("_method"))
+
+app.get("/", checkAuthenticated, (req, res) => {
+    res.render("index.ejs" , {name: req.user.name })
 })
 
 app.get("/login", checkNotAuthenticated, (req,res) => {
     res.render("login.ejs")
 })
 
-app.post("/login", checkNotAuthenticated , passport.use (" new CustomStartegy", {
+app.post("/login", checkNotAuthenticated , passport.authenticate ("local", {
     successRedirect: "/",
     failureRedirect: "/login",
     failureFlash: true
 }))
 
-app.get("/register", checkNotAuthenticated , (req,res) => {
+app.get("/register", checkNotAuthenticated, (req,res) => {
     res.render("register.ejs")
 })
 
@@ -52,7 +65,6 @@ app.post("/register", checkNotAuthenticated, async (req, res) => {
         
     } catch {
         res.redirect("/register")
-    
     }   console.log(users)
 })
 
@@ -61,7 +73,7 @@ app.delete("/logout", (req, res) => {
     res.redirect("/login")
 })
 
-function checkAuthentication(req, res, next) {
+function checkAuthenticated(req, res, next) {
     if (req.isAuthenticated()) {
         return next()
     }
@@ -69,7 +81,7 @@ function checkAuthentication(req, res, next) {
 }
 function checkNotAuthenticated(req, res, next) {
     if (req.isAuthenticated()) {
-       res.redirect("/")
+      return res.redirect("/")
     }
     next()
 }
