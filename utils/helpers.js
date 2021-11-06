308bc885fa453b3e36ea328f2ceb602caa737ae2
Steve Bendrick <steven.bendrick@gmail.com>
2021-11-06 15:40:24 -0400

    updated emoji styling and fixed incorrect count bug

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,5 +1,13 @@
 module.exports = {
   format_date: date => {
     return `${new Date(date).getMonth() +1}/${new Date(date).getDate()}/${new Date(date).getFullYear()}`;
+  },
+  reactionCount: (reactionArray, reactionNum) => {
+    const reactionObj = reactionArray.filter(reaction => reaction.reaction_id === parseInt(reactionNum));
+    if (reactionObj.length === 0) {
+      return 0;
+    } else {
+      return reactionObj[0].reactionCount;
+    }
   }
 }
