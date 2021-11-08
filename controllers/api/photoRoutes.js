0a9671eafcf77a06b3b0f2971859f012e439abb9
e1m3m0 <gamateo@gmail.com>
2021-11-07 20:54:19 -0500

    added eventlistener working on fucntion to upload

--- controllers/api/photoRoutes.js
+++ controllers/api/photoRoutes.js
@@ -1,14 +1,19 @@
-const router = require('express').Router() 
+const router = require('express').Router();
 const aws = require('aws-sdk');
 const s3 = new aws.S3();
 const multer = require('multer');
 const multerS3 = require('multer-s3');
+const bodyParser = require('body-parser');
+var photo; 
 
 aws.config.update({
   accessKeyId: process.env.AWS_ACCESS_KEY,
   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
   region: process.env.AWS_BUCKET_REGION
 });
+
+
+router.use(bodyParser.json);
 
 const fileFilter = (req, file, cb) => {
   if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
@@ -18,7 +23,7 @@ const fileFilter = (req, file, cb) => {
   }
 }
 
-const upload = multer({
+const upload = multer({  
   fileFilter,
   storage: multerS3({
     s3: s3,
@@ -30,14 +35,67 @@ const upload = multer({
   })
 });
 
-router.post('/upload', upload.array('upl', 1), function(req, res, next) {
-  const photo = req.files[0].key;
+router.post('/api/photos', upload.array('upl', 1), function(req, res, next) {
+  console.log('here in multer');
+  photo = req.files[0].key;
 
   console.log(photo);
 
-  res.send(photo)
-})
+  displayPhoto()
 
+  .then((img) => {
+
+    let image = "<img src='data:image/jpeg;base64," + encode(img.Body) + "'" + "/>";
+    let startHTML = "<html><body></body>";
+    let endHTML = "</body></html>";
+    let html = startHTML + image + endHTML;
+    res.send(html)
+  })//.catch((e)=>{
+//     res.send(e)
+//})
+  
+})
+// var bucket = new aws.S3({ params: { Bucket: 'pepweb' } });
+// bucket.listObjects(function (err, data) {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log(data);
+//     router.get.allImageData = data.Contents;
+//   }
+// });
+
+
+
+async function displayPhoto() {
+  /*   aws.config.update({
+     accessKeyId: process.env.AWS_ACCESS_KEY, secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY});
+     aws.config.region = process.env.AWS_BUCKET_REGION;
+     var bucket = new aws.S3({params: {Bucket: 'pepweb'}});
+     console.log("hello")
+     bucket.getObject({Key: picturename},function(err,file){
+     $timeout(function(){
+         app.s3url = "dbodyata:image/jpeg;base64," + encode(file.Body);
+     },1);
+     
+ });
+ }*/
+
+  let bucket = new aws.S3();
+
+  const data = bucket.getObject({
+    Bucket: 'pepweb',
+    Key: photo
+  }).promise();
+  return data;
+}
+
+
+function encode(data) {
+  let buf = Buffer.from(data);
+  let base64 = buf.toString('base64');
+  return base64
+}
 
 
 
--- public/javascript/addPhoto.js
+++ public/javascript/addPhoto.js
@@ -1,36 +1,37 @@
-async function addPhoto(event) {
+function addPhoto(event) {
   event.preventDefault();
 
   const input = document.querySelector("input[type='file']");
   const user_id = document.querySelector(".user-data").getAttribute("id").trim();
   const post_id = window.location.toString().split("/")[window.location.toString().split("/").length - 1];
+  console.log(input, user_id, post_id)
 
   const formData = new FormData();
   formData.append('file', input.files[0])
   formData.append('name', 'upl')
+  console.log(formData)
   
-  const post_photo = document.querySelector('.photo');
-  post_photo.setAttribute('id') = input.files[0].lastModified;
+  console.log(input.files);
 
   // console.log(input.files[0])
 
   // let data = new FormData();
   // data.append('file', input.files[0]);
 
-  const response = await fetch("/api/photos", {
-    method: "POST",
-    body: formData,
-    headers: {
-      'Content-Type': 'multipart/form-data'
-    },
-  })
+  // const response = fetch("/api/photos", {
+  //   method: "POST",
+  //   body: formData,
+  //   headers: {
+  //     'Content-Type': 'multipart/form-data'
+  //   },
+  // })
 
-  if (response.ok) {
-    console.log(response.json);
+  // if (response.ok) {
+  //   console.log(response.json);
 
-  } else {
-    alert(response.statusText);
-  }
+  // } else {
+  //   alert(response.statusText);
+  // }
 }
 
-// document.querySelector('.photo-submit').addEventListener('submit', addPhoto);
+document.querySelector('.photo-submit').addEventListener('submit', addPhoto);
