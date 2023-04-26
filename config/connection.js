f1f68314e933f364092df6a619006dd14e31bad2
e1m3m0 <gamateo@gmail.com>
2023-04-26 19:43:10 -0400

    trying clear database in heroku

--- config/connection.js
+++ config/connection.js
@@ -3,8 +3,8 @@ const Sequelize = require('sequelize');
 require('dotenv').config();
 
 // create connection to our db
-const sequelize = process.env.JAWSDB_URL
-  ? new Sequelize(process.env.JAWSDB_URL)
+const sequelize = process.env.CLEARDB_JADE_URL
+  ? new Sequelize(process.env.CLEARDB_JADE_URL)
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
       host: 'localhost',
       dialect: 'mysql',
@@ -12,3 +12,6 @@ const sequelize = process.env.JAWSDB_URL
     });
 
 module.exports = sequelize;
+
+
+// mysql://b532abba556d55:c581a433@us-cdbr-east-06.cleardb.net/heroku_86d1a935ed779fc?reconnect=true
